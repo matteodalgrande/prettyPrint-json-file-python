e8109cb76af4588dacf969e9e5924f2d0d383860
Matteo Dal Grande <matteodalgrande97@gmail.com>
2020-05-21 08:58:19 +0200

    Update read-write-json-file.py

--- read-write-json-file.py
+++ read-write-json-file.py
@@ -1,10 +1,10 @@
 ################################# APRIRE FILE, LEGGERE, SCRIVERE E SOVRASCRIVERLO###########################################
 import json
-with open('gl-dast-report.json', 'r') as f:
+with open('zap_full_scan_report.json', 'r') as f:
     t = f.read()
     parsed_json = json.loads(str(t))
     indent_json = json.dumps(parsed_json, indent=4, sort_keys=True)
     f.close()
-with open('gl-dast-report.json', 'w+') as f:
+with open('zap_full_scan_report.json', 'w+') as f:
     f.write(indent_json)
     f.close()
